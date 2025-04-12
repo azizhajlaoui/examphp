efaad4327d2729307f90fdc6983cf3e828e0fa74
azizhajlaoui <93492402+azizhajlaoui@users.noreply.github.com>
2025-04-12 20:45:30 +0100

    Update email_config.php

--- email_config.php
+++ email_config.php
@@ -1,18 +1,1 @@
-<?php
-// Gmail SMTP Configuration
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'azizhajlaoui2@gmail.com');
-define('SMTP_PASSWORD', 'ogpzgwkpwsgzajxv');
-define('SMTP_FROM_EMAIL', 'azizhajlaoui2@gmail.com');
-define('SMTP_FROM_NAME', 'examphp');
 
-// Enable TLS
-define('SMTP_SECURE', 'tls');
-
-// Additional PHP mail settings
-ini_set("SMTP", SMTP_HOST);
-ini_set("smtp_port", SMTP_PORT);
-ini_set("sendmail_from", SMTP_FROM_EMAIL);
-ini_set("smtp_ssl", "tls");
-?> 
